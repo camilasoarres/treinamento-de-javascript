abbfeeece3daf123668f5021edf1e058e5a042e8
Camila Soares <camilasoares@1sti.com.br>
2019-06-26 17:58:22 -0300

    refactor code and pomodoro running

--- projetos/pomodoro/js-version/scripts.js
+++ projetos/pomodoro/js-version/scripts.js
@@ -1,48 +1,67 @@
+let container = document.getElementById('container');
+let cronometro = document.getElementById('cronometro');
+let messagem = document.getElementById('messagem');
 
-let $cronometro = document.getElementById('#cronometro');
-let $mensagem = document.getElementById('#mensagem');
-
-let pomodoro = 10;
-let intervalo = 5;
-let running;
 let currentType;
+let currentTime;
+let running;
+let rendering;
+let pomodoroTime = 1500;
+let intervalTime = 300;
+let completedPomodoro = 0;
 
-function rodar() {
-	running = setInterval(function() { startPomodoro(); }, 1000);
-}
-
-function reset() {
-	pomodoro = 0;
-	clearInterval(running);
-	cronometro.innerHTML = pomodoro;
-}
-
-function parar() {
-	clearInterval(running);
-}
-
-function formatTime() {
-	let minutos = Math.floor(pomodoro % 3600 / 60);
-	let segundos = Math.floor(pomodoro % 3600 % 60);
-
-	return (minutos < 10 ? "0" : "") + minutos + ":" + (segundos < 10 ? "0" : "") + segundos;
-}
-
-function updateDom() {
-	cronometro.innerHTML = formatTime();
+function changeType(type) {
+  if (type === 'pomodoro') {
+    currentTime = pomodoroTime;
+  }
+  if (type === 'intervalo') {
+    currentTime = intervalTime;
+  }
+  currentType = type;
+  cronometro.value = formatTime(cronometro.value);
 }
 
 function startPomodoro() {
-	if (pomodoro > 0) {
-		pomodoro = pomodoro - 1;
-		updateDom();
-	}
-	else if (pomodoro == 0) {
-		mensagem.innerHTML = 'o pomodoro terminou, começou o intervalo!';
-		pomodoro = 5;
-	}
-	else {
-		return clearInterval(running);
-	}
+  if (!running) {
+    running = true;
+    container.classList.add('running');
+    rendering = setInterval(function() {
+      startRendering(cronometro.value)
+    }, 1000);
+  }
 }
 
+function startRendering() {
+  container.classList.add('running');
+  if (currentTime > 0) {
+    currentTime = currentTime - 1;
+    cronometro.value = formatTime();
+  }
+  else if (currentTime === 0 && currentType == 'pomodoro') {
+    changeType('intervalo');
+    completedPomodoro = completedPomodoro + 1;
+  }
+  else if (completedPomodoro >= 4) {
+    clearInterval(rendering);
+    running = false;
+    messagem.innerHTML = "Este é o " + completedPomodoro + "º pomodoro finalizado!";
+    completedPomodoro = 0;
+  }
+  else {
+    changeType('pomodoro');
+    startPomodoro();
+  }
+}
+
+function formatTime() {
+  let minutes = Math.floor(currentTime % 3600 / 60);
+  let seconds = Math.floor(currentTime % 3600 % 60);
+
+  return (minutes < 10 ? "0" : "") + minutes + ':' + (seconds < 10 ? "0" : "") + seconds
+}
+
+function pausePomodoro() {
+  running = false
+  container.classList.remove('running');
+  clearInterval(rendering)
+}
