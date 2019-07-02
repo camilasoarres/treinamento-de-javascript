c225473adb5b200a038404cdc519f2217ca3bb91
Camila Soares <camilasoares@1sti.com.br>
2019-07-02 17:39:03 -0300

    clean code and change how to edit a task

--- projetos/todoList/js-version/scripts.js
+++ projetos/todoList/js-version/scripts.js
@@ -1,79 +1,102 @@
-let taskInput = document.querySelector('#task-input');
-let todoPrincipalList = document.querySelector('.todo-list ul');
-let todoCompletedList =  document.querySelector('.finish-items ul');
-let descriptionInput = document.querySelector('#description-input');
-let categoryInput = document.querySelector('#category-input');
-let colorInput = document.querySelector('#color-input');
-
-function createNewTask(task, description, category, color) {
-  let listItem = document.createElement('li');
-  let checkBox = document.createElement('input');
-  let labelItem = document.createElement('label');
-  let categoryItem = document.createElement('p');
-
-  labelItem.innerText = task + ' - ' + category;
-  categoryItem.innerText = description;
-  checkBox.type = 'checkbox';
-  listItem.appendChild(checkBox);
-  listItem.appendChild(labelItem);
-  listItem.appendChild(categoryItem);
-
-  listItem.style.backgroundColor = color;
-
-  return listItem;
-};
+let form = document.querySelector("form");
+let todoList = document.createElement("ul");
+let tasks = [];
+let isEditing = undefined;
 
 function addTask() {
-  let listItem = createNewTask(taskInput.value, descriptionInput.value, categoryInput.value, colorInput.value);
+	event.preventDefault();
 
-  if (taskInput.value !== '') {
-    todoPrincipalList.appendChild(listItem); 
-    taskInput.value = '';
-    descriptionInput.value = '';
-    categoryInput.value = '';
-    colorInput.value = '#800000';
-  }
+	if (typeof(isEditing) == 'number') {
+		editTask();
+	}
 
-  principalTodoItems(listItem, finishTask);
+	else {
+		tasks.push({
+			title: form.elements.title.value,
+			description: form.elements.description.value,
+			category: form.elements.category.value,
+			color: form.elements.color.value,
+		});
+
+		renderTasks();
+		cleanValues();
+	}
 };
 
-function finishTask() {
-  let listItem = this.parentNode;
-  let deleteBtn = document.createElement('button');
+function editTask() {
+	tasks[isEditing] = {
+		title: form.elements.title.value,
+		description: form.elements.description.value,
+		category: form.elements.category.value,
+		color: form.elements.color.value,
+	};
 
-  deleteBtn.innerText ='Deletar'; 
-  deleteBtn.className = 'delete';
-  listItem.className = 'finished-task';
-  listItem.appendChild(deleteBtn);
-
-  let checkBox = listItem.querySelector('input[type=checkbox]');
-  checkBox.remove();
-  
-  todoCompletedList.appendChild(listItem);
-  completedTodoItems(listItem, deleteTask);
-};
-
-function deleteTask() {
-  let listItem = this.parentNode;
-  let ul = listItem.parentNode;
-
-  ul.removeChild(listItem);
-};
-
-function principalTodoItems(taskItem, checkBoxClick) {  
-  let checkBox = taskItem.querySelector('input[type=checkbox]');
-  checkBox.onchange = checkBoxClick;
-};
-
-function completedTodoItems (taskItem, deleteButtonPress) {  
-  let deleteButton = taskItem.querySelector('.delete');
-  deleteButton.onclick = deleteButtonPress;
-};
-
-for(let i = 0; i < todoPrincipalList.children.length; i++) {
-  principalTodoItems(todoPrincipalList.children[i], finishTask);
+	isEditing = undefined;
+	cleanValues();
+	renderTasks();
 }
 
-for(let i = 0; i < todoCompletedList.children.length; i++) {
-  completedTodoItems(todoCompletedList.children[i], deleteTask);
+function renderTasks() {
+	while (todoList.firstChild) {
+		todoList.removeChild(todoList.firstChild);
+	}
+
+	tasks.map((task, index) => {
+		let todoItem = document.createElement("li");
+		let title = renderItem("p", task.title);
+		let description = renderItem("p", task.description);
+		let category = renderItem("p", task.category);
+		let buttons = document.createElement("div");
+		let buttonEdit = renderItem("button", "edit");
+		let buttonDelete = renderItem("button", "delete");
+
+		todoItem.appendChild(title);
+		todoItem.appendChild(description);
+		todoItem.appendChild(category);
+		todoItem.appendChild(buttons);
+		buttons.appendChild(buttonDelete);
+		buttons.appendChild(buttonEdit);
+
+		todoItem.style.backgroundColor = task.color;
+
+		todoList.appendChild(todoItem);
+
+		buttonDelete.addEventListener("click", function() {
+			tasks = deleteTask(tasks, task);
+			renderTasks();
+		});
+
+		buttonEdit.addEventListener("click", function() {
+			form.elements.description.value = task.description;
+			form.elements.category.value = task.category;
+			form.elements.title.value = task.title;
+			form.elements.color.value = task.color;
+			isEditing = index;
+		});
+
+	});
+};
+
+function deleteTask(tasks, task) {
+	return tasks.filter(function(el){
+		return el != task;
+	});
 }
+
+function renderItem(el, txt) {
+	let listItem = document.createElement(el);
+	let itemText = document.createTextNode(txt);
+
+	listItem.appendChild(itemText)
+
+	return listItem;
+}
+
+function cleanValues() {
+	form.elements.description.value = '';
+	form.elements.category.value = '';
+	form.elements.title.value = '';
+	form.elements.color.value = "#800000";
+}
+
+document.body.appendChild(todoList);
